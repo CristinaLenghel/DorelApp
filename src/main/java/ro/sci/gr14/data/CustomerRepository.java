d12bac6455f5ccb1d3812a1c1e5a1af6905be6fb
laurentiulorincz <lorinczmarius@hotmail.com>
2019-05-27 22:23:44 +0300

    added data

---
+++ src/main/java/ro/sci/gr14/data/CustomerRepository.java
@@ -0,0 +1,28 @@
+package ro.sci.gr14.data;
+
+import ro.sci.gr14.model.Customer;
+
+import java.util.List;
+
+public class CustomerRepository implements RepositoryInterface<Customer> {
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public Customer getById(long Id) {
+        return null;
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer delete(long Id) {
+        return null;
+    }
+}
