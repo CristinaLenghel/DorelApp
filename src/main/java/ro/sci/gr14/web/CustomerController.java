7f324f0b0ece3ac56514fc09b13a6cb0fb4c617a
laurentiulorincz <lorinczmarius@hotmail.com>
2019-06-04 20:05:05 +0300

    some change

--- src/main/java/ro/sci/gr14/web/CustomerController.java
+++ src/main/java/ro/sci/gr14/web/CustomerController.java
@@ -1,7 +1,11 @@
 package ro.sci.gr14.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,25 +21,20 @@ public class CustomerController {
     @Autowired
     IBaseUserRepository buRepo;
 
-
     @GetMapping
     public String saveUser(Model model) {
-//        BaseUser baseUser=new BaseUser();
-//        baseUser.setUserName("userName");
-//
-//        buRepo.save(baseUser);
 
         BaseUser baseUser=buRepo.findById(1L).orElse(new BaseUser());
         model.addAttribute("myUser",baseUser);
         System.out.println(baseUser);
 
-//                .ifPresent(bs -> {
-//                    log.info("Customer found with findById(1L):");
-//                    log.info("--------------------------------");
-//                    log.info(bs.toString());
-//                    log.info("");
-//                });
-
         return "user";
+    }
+
+    @Bean
+    public CommandLineRunner demo() {
+        return (args) -> {
+            buRepo.save(new BaseUser(12L, "laurentiu", "steaua", "marius@gmail.com", "marius", "012312", "aiud", "alba", "sal"));
+        };
     }
 }
