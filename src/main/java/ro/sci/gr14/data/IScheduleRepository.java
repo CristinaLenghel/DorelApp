38b54c0e0f44ea57bd1c1857956d8d1c9efad2c6
laurentiulorincz <lorinczmarius@hotmail.com>
2019-07-06 13:31:40 +0300

    am sters directorul src

--- src/main/java/ro/sci/gr14/data/IScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.sci.gr14.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ro.sci.gr14.model.Schedule;
-
-@Repository
-public interface IScheduleRepository extends CrudRepository<Schedule, Long> {
-}
--- src/main/java/ro/sci/gr14/security/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package ro.sci.gr14.security;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-
-    void autoLogin(String username, String password);
-}
